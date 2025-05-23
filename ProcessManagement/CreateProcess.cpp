7080e69f73a36b4cbabb4d2f15d2f6dfd570bea5
Hazerakteritu <hazerakteritu@gmail.com>
2025-05-23 22:50:06 +0600

    fork()

---
+++ ProcessManagement/CreateProcess.cpp
@@ -0,0 +1,28 @@
+//Using fork 
+//getpid() returns the current process ID.
+//getppid() returns the parent process ID.
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t q;
+    printf("Before fork\n");
+    q = fork();
+
+    if(q < 0)
+        printf("Error\n");
+
+    else if(q == 0) {
+        printf("I am a Child having pid %d\n", getpid());
+        printf("My Parent's pid is   %d\n", getppid());
+    }
+    else {
+        wait(NULL);  // Wait for child to finish
+        printf("Parent having  pid is   %d\n", getpid());
+        printf("My Child's pid is   %d\n", q);
+    }
+}

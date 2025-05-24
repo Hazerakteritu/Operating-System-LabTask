e84bbaef44d54657738381ff6bc0564e335cdd71
Hazerakteritu <hazerakteritu@gmail.com>
2025-05-24 19:24:09 +0600

    Parameter passing

---
+++ Thread/parameterPass.cpp
@@ -0,0 +1,28 @@
+//Parameter Passing 
+// program to create thread. Thread prints 0-4 while the main process prints 20-24 
+
+#include<stdio.h> 
+#include<unistd.h> 
+#include<pthread.h> 
+
+void *thread1_func(void *arg); 
+int i,n, j; 
+
+int main(){ 
+    pthread_t a;    
+    void *result; 
+    int num[2] = {3, 5}; 
+    pthread_create(&a, NULL, thread1_func, (void *)num);  
+    pthread_join(a, &result);    // process waits for thread to finish 
+    printf("Inside Main Process\n"); 
+    printf("Thread1 returned: %s\n", (char *)result); 
+} 
+ 
+void *thread1_func(void *arg){ 
+    printf("Inside Thread1\n"); 
+ 
+    int *x = arg; 
+    int sum = x[0] + x[1]; 
+    printf("Sum is %d\n", sum); 
+    pthread_exit("Sum calculated"); 
+}

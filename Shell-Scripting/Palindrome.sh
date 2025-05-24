c74235eace5d4bc0ba5f72b6a2f00b8f043bd4e1
Hazerakteritu <hazerakteritu@gmail.com>
2025-05-24 22:11:11 +0600

    Check if a string Palindrome or not

--- Shell-Scripting/Palindrome.sh
+++ Shell-Scripting/Palindrome.sh
@@ -1,8 +1,11 @@
 #Check if a String is Palindrome or not
 
 #!/bin/bash
-read -p "Enter a string: " str
+
+read -p "Enter a String: " str
+
 rev=$(echo $str | rev)
+
 if [ "$str" == "$rev" ]; then
     echo "Palindrome"
 else
